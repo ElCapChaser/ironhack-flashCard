c84613812a2d0862a4578af21804825841b4a12d
Steven Verlinden <stevenverlinden5@gmail.com>
2021-02-06 15:18:44 +0100

    updated sign-up form and user model

--- models/user.js
+++ models/user.js
@@ -2,20 +2,37 @@
 
 const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      required: true
+    },
+    passwordHashAndSalt: {
+      type: String,
+      required: true
+    },
+    fullTime: {
+      type: Boolean,
+      required: true
+    },
+    startDate: {
+      type: Date,
+      required: true
+    },
+    profilePicture: {
+      type: String
+    }
   },
-  email: {
-    type: String,
-    required: true,
-    lowercase: true,
-    trim: true
-  },
-  passwordHashAndSalt: {
-    type: String
-  }
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('User', schema);
