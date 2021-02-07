04aea90f8f5fe948e2e020dc4a9a8540f2f2b0a6
asaskiap <alannapfeiffer@gmail.com>
2021-02-07 13:31:54 +0100

    added create but still without file upload

--- routes/flashcard.js
+++ routes/flashcard.js
@@ -5,8 +5,37 @@ const Flashcard = require('./../models/flashcard');
 
 const router = new express.Router();
 
+// create a new flashcard
 router.get('/create', (req, res, next) => {
     res.render('flashcards/create');
+});
+
+router.post('/create', (req, res, next) => {
+    console.log(req);
+    const data = req.body;
+
+    //   let image;
+    //   if (req.file) {
+    //     image = req.file.path;
+    //   }
+    console.log(data);
+
+    Flashcard.create({
+            questionTitle: data.title,
+            questionDescription: data.description,
+            solution: data.solution,
+            topic: data.topic,
+            module: data.module,
+            difficulty: data.difficulty
+                //creator: req.user._id
+                // creator: req.session.userId
+        })
+        .then((flashcard) => {
+            res.redirect(`/flashcard/${flashcard._id}`);
+        })
+        .catch((error) => {
+            next(error);
+        });
 });
 
 router.get('/:id', (req, res, next) => {
--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ const router = new express.Router();
 const routeGuard = require('./../middleware/route-guard');
 
 router.get('/', (req, res, next) => {
-  res.render('home', { title: 'Hello World!' });
+    res.render('home', { title: 'Iron Flashcards!' });
 });
 
 router.get('/private', routeGuard, (req, res, next) => {
-  res.render('private');
+    res.render('private');
 });
 
 module.exports = router;
