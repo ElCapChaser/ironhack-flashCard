43da9b994888ddee4b16961eda75e8f29c76b02a
Steven Verlinden <stevenverlinden5@gmail.com>
2021-02-09 19:42:32 +0100

    Updated auth + added comments

--- app.js
+++ app.js
@@ -14,40 +14,45 @@ const bindUserToViewLocals = require('./middleware/bind-user-to-view-locals.js')
 const baseRouter = require('./routes/index');
 const authenticationRouter = require('./routes/authentication');
 const flashcardRouter = require('./routes/flashcard');
+const commentsRouter = require('./routes/newComment');
+const hbs = require('hbs');
 
 const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(serveFavicon(path.join(__dirname, 'public/images', 'favicon.ico')));
 app.use(
-    sassMiddleware({
-        src: path.join('styles'),
-        dest: path.join(__dirname, 'public/styles'),
-        prefix: '/styles',
-        outputStyle: process.env.NODE_ENV === 'development' ? 'expanded' : 'compressed',
-        force: process.env.NODE_ENV === 'development',
-        sourceMap: process.env.NODE_ENV === 'development'
-    })
+  sassMiddleware({
+    src: path.join('styles'),
+    dest: path.join(__dirname, 'public/styles'),
+    prefix: '/styles',
+    outputStyle:
+      process.env.NODE_ENV === 'development' ? 'expanded' : 'compressed',
+    force: process.env.NODE_ENV === 'development',
+    sourceMap: process.env.NODE_ENV === 'development'
+  })
 );
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(
-    expressSession({
-        secret: process.env.SESSION_SECRET,
-        resave: true,
-        saveUninitialized: false,
-        cookie: {
-            maxAge: 15 * 24 * 60 * 60 * 1000,
-            httpOnly: true
-        },
-        store: new(connectMongo(expressSession))({
-            mongooseConnection: mongoose.connection,
-            ttl: 24 * 60 * 60
-        })
+  expressSession({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 15 * 24 * 60 * 60 * 1000,
+      httpOnly: true
+    },
+    store: new (connectMongo(expressSession))({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60
     })
+  })
 );
 app.use(basicAuthenticationDeserializer);
 app.use(bindUserToViewLocals);
@@ -55,19 +60,20 @@ app.use(bindUserToViewLocals);
 app.use('/', baseRouter);
 app.use('/authentication', authenticationRouter);
 app.use('/flashcard', flashcardRouter);
+app.use('/', commentsRouter);
 
 // Catch missing routes and forward to error handler
 app.use((req, res, next) => {
-    next(createError(404));
+  next(createError(404));
 });
 
 // Catch all error handler
 app.use((error, req, res, next) => {
-    // Set error information, with stack only available in development
-    res.locals.message = error.message;
-    res.locals.error = req.app.get('env') === 'development' ? error : {};
-    res.status(error.status || 500);
-    res.render('error');
+  // Set error information, with stack only available in development
+  res.locals.message = error.message;
+  res.locals.error = req.app.get('env') === 'development' ? error : {};
+  res.status(error.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
